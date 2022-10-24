d982036006b794482bf113f0c45062f3d53864af
babyguy <mangatomax@gmail.com>
2022-10-24 07:03:26 -0500

    cambio de direccion del modal de la pagina1

--- pag1/modal_pag1.js
+++ pag1/modal_pag1.js
@@ -1,10 +1,10 @@
 // se localiza el modal gracias al id
-const modal = document.getElementById('modal');
+const modal = document.getElementById("modal");
 // se muestra el modal cambiando su propiedad style = toip 0 left 0
-modal.style = "  top: 0 ; left: 0";
+modal.style = "  top: 0 ; left: 0; transition: all 0.5s;";
 
-// se detecta el click en el boton opara cerrar el modal 
-document.getElementById('btn-cerrar-modal').addEventListener('click', ()=>{
-// se cierrra el modal cambiando su propiedad style = toip -100vh
-   modal.style = "  top: -100vh; ";
+// se detecta el click en el boton opara cerrar el modal
+document.getElementById("btn-cerrar-modal").addEventListener("click", () => {
+  // se cierrra el modal cambiando su propiedad style = toip -100vh
+  modal.style = "  left: -100vh; top: 0; transition: all 0.5s ";
 });
